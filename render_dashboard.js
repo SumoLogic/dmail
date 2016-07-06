095e17c7922e824cb96f41fb78860933f925c896
Christian Beedgen <christian@sumologic.com>
2016-07-05 19:01:48 -0700

    So, delete is getting called multiple times...

--- render_dashboard.js
+++ render_dashboard.js
@@ -57,82 +57,129 @@ casper.log("Filename    : " + filename);
 //
 
 casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
-casper.options.waitTimeout = 120000;
+casper.options.waitTimeout = 5 * 60000;
 
 
 //
 // Hook up request & response monitoring.
 //
 
+var removedSIDs = {};
+var addedSIDs = {};
 
-//
-// Hook up request & response monitoring.
-//
+casper.options.onResourceRequested = function (C, requestData, networkRequest) {
 
-casper.options.onResourceRequested = function(C, requestData, networkRequest) {
+    function parseSIDs(s) {
+        var re = /sid=(.*?)(&|$)/g;
+        var match;
+        var sids = [];
+        while (match = re.exec(s)) {
+            sids.push(match[1]);
+        }
+        return sids;
+    }
+
     // casper.log('[DMAIL]     >>>> Request to: ' + requestData.url, 'debug');
-	if (requestData.url.indexOf('sumologic') != -1) {
-		if (requestData.url.indexOf('sessionids') != -1) {
-			if (requestData.method == 'DELETE') {
-			    casper.log('[DMAIL] Session IDs deleted', 'info');				
-				casper.evaluate(function() {
-					var div = document.createElement('div');
-					div.id = 'overanddone';
-					document.body.insertBefore(div, document.body.firstChild);
-				});
-			}
-		}
-	    // casper.log('    >>>> Request ' + JSON.stringify(requestData, undefined, 4), 'debug');
+    try {
+        if (requestData.url.indexOf('sumologic') != -1) {
+            if (requestData.url.indexOf('sessionids') != -1) {
+                var sids = parseSIDs(requestData.url);
+                var i;
+                var sid;
+                if (requestData.method == 'GET') {
+                    casper.log("[DMAIL] GET SIDs: " + sids);
+                    for (i = 0; i < sids.length; i++) {
+                        sid = sids[i];
+                        if (removedSIDs[sid]) {
+                            casper.log("[DMAIL] SID was previously removed: " + sid, 'debug');
+                        } else {
+                            if (!addedSIDs[sid]) {
+                                casper.log("[DMAIL] Adding SID: " + sid, 'debug');
+                                addedSIDs[sid] = sid;
+                            }
+                        }
+                        casper.log("[DMAIL] addedSIDs: " + JSON.stringify(addedSIDs) +
+                            ", removedSIDs: " + JSON.stringify(removedSIDs));
+                    }
+                } else if (requestData.method == 'DELETE') {
+                    casper.log("[DMAIL] DELETE SIDs: " + sids);
+                    for (i = 0; i < sids.length; i++) {
+                        sid = sids[i];
+                        casper.log("[DMAIL] Removing SID: " + sid, 'debug');
+                        removedSIDs[sid] = sid;
+                        delete addedSIDs[sid];
+                    }
+                    casper.log("[DMAIL] addedSIDs: " + JSON.stringify(addedSIDs) +
+                        ", removedSIDs: " + JSON.stringify(removedSIDs));
+                    var addedSIDsLength = Object.keys(addedSIDs).length;
+                    var removedSIDsLength = Object.keys(removedSIDs).length;
+                    casper.log("[DMAIL] addedSIDs: " + addedSIDsLength +
+                        ", removedSIDs: " + removedSIDsLength);
+                    if (addedSIDsLength < 1 && removedSIDsLength > 0) {
+                        casper.log('[DMAIL] All session IDs deleted', 'info');
+                        casper.evaluate(function () {
+                            var div = document.createElement('div');
+                            div.id = 'overanddone';
+                            document.body.insertBefore(div, document.body.firstChild);
+                        });
+                    }
+                }
+            }
+            // casper.log('    >>>> Request ' + JSON.stringify(requestData, undefined, 4), 'debug');
+        }
+    } catch (err) {
+        casper.log(err, 'error');
     }
 };
-casper.options.onResourceReceived = function(C, response) {
-	if (response.stage == 'end') {
-		// casper.log('[DMAIL]     <<<< Response from: ' + response.url, 'debug');
-		if (response.url.indexOf('sumologic') != -1) {
-			// casper.log('[DMAIL]     <<<< Response ' + JSON.stringify(response, undefined, 4), 'debug');
-		}
-	}
+
+casper.options.onResourceReceived = function (C, response) {
+    if (response.stage == 'end') {
+        // casper.log('[DMAIL]     <<<< Response from: ' + response.url, 'debug');
+        if (response.url.indexOf('sumologic') != -1) {
+            // casper.log('[DMAIL]     <<<< Response ' + JSON.stringify(response, undefined, 4), 'debug');
+        }
+    }
 };
 
 //
 // Put together the plan to execute.
 //
 
-var dashboardUrl = url + "/ui/dashboard.html?f=" + + dashboardId + "&t=r";
-casper.start(dashboardUrl, function() {
-	casper.log("[DMAIL] Started with URL: " + dashboardUrl, 'info');
+var dashboardUrl = url + "/ui/dashboard.html?f=" + +dashboardId + "&t=r";
+casper.start(dashboardUrl, function () {
+    casper.log("[DMAIL] Started with URL: " + dashboardUrl, 'info');
 });
-casper.waitForSelector('#input-email', function() {
-	casper.log("[DMAIL] Got selector #input-email, now filling login form...", 'info');
-	this.fill('form#form-login',
-	 {
-	    'login': username,
-    	'password': password
-    }, 
-	true);
+casper.waitForSelector('#input-email', function () {
+    casper.log("[DMAIL] Got selector #input-email, now filling login form...", 'info');
+    this.fill('form#form-login',
+        {
+            'login': username,
+            'password': password
+        },
+        true);
 });
-casper.waitForSelector('.iris-content', function() {
-	casper.log("[DMAIL] Got selector: .iris-content, now waiting for sessionids to be deleted...", 'info');
-	lastSessionsIdsCall = Date.now();
+casper.waitForSelector('.iris-content', function () {
+    casper.log("[DMAIL] Got selector: .iris-content, now waiting for sessionids to be deleted...", 'info');
+    lastSessionsIdsCall = Date.now();
 });
-casper.waitForSelector('div#overanddone', function() {
-	this.wait(5000, function() {
-		try {
-		    var documentHeight = casper.evaluate(function() { 
-		        return document.body.offsetHeight; 
-		    });
-		    casper.log("[DMAIL] Document height: " + documentHeight);
-			casper.captureSelector(filename, '.iris-content', {
-				top: 0,
-				left: 0,
-				width: DOCUMENT_WIDTH, 
-	        	height: 810
-			});
-			casper.log("[DMAIL] Caputred screen: " + filename, 'info');
-		} catch (err) {
-			casper.log("ERROR " + err);
-		}
-	});
+casper.waitForSelector('div#overanddone', function () {
+    this.wait(5000, function () {
+        try {
+            var documentHeight = casper.evaluate(function () {
+                return document.body.offsetHeight;
+            });
+            casper.log("[DMAIL] Document height: " + documentHeight);
+            casper.captureSelector(filename, '.iris-content', {
+                top: 0,
+                left: 0,
+                width: DOCUMENT_WIDTH,
+                height: 810
+            });
+            casper.log("[DMAIL] Caputred screen: " + filename, 'info');
+        } catch (err) {
+            casper.log("ERROR " + err);
+        }
+    });
 });
 
 
